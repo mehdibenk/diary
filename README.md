39d5c90064edaed017e49b382ad1273e461ac918
Mehdi Benkirane <mehdi@localhost.localdomain>
2015-09-05 21:44:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# diary
+This is a diary application written in c++.
+It uses the Qt 5 toolkit.
+
---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+
+void MainWindow::on_actionNewTab_triggered()
+{
+	QWidget *diaryPage = new QWidget();
+	QGridLayout *diaryPageLayout = new QGridLayout();
+	QTextEdit *diaryPageEditor = new QTextEdit();
+	diaryPageLayout->addWidget(diaryPageEditor);
+
+
+	diaryPage->setLayout(diaryPageLayout);
+
+	ui->tabWidget->insertTab(ui->tabWidget->count(), diaryPage, "New &Page");
+}
